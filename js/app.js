ff8dfd1df279b242211de3f70423259a4183614b
brent crystal <brentrcrystal@gmail.com>
2026-02-21 10:12:48 -0500

    Progress Commit: Refactoring html, css, javascript

--- js/app.js
+++ js/app.js
@@ -24,24 +24,34 @@
         }
     }
 
-   function updateResume(){
-    const header = document.querySelector('.tagline');
-    header.innerText = app.resumeItems.header.tagline;
+    function updateResume(){
+        const header = document.querySelector('.tagline');
+        header.innerText = app.resumeItems.header.tagline;
 
-    const summary = document.querySelector('.summary-text');
-    summary.innerText = app.resumeItems.summary;
+        const summary = document.querySelector('.summary-text');
+        summary.innerText = app.resumeItems.summary;
 
-    const skillsContainer = document.getElementById('skills-container');
-    skillsContainer.innerHTML = app.resumeItems.skills.map(category => `
-        <div class="skill-category">
-            <h3>${category.category}</h3>
-            <div class="skill-pills">
-                ${category.items.map(skill => `<span class="pill">${skill}</span>`).join('')}
+        const skillsContainer = document.getElementById('skills-container');
+        skillsContainer.innerHTML = app.resumeItems.skills.map(category => `
+            <div class="skill-category">
+                <h3>${category.category}</h3>
+                <div class="skill-pills">
+                    ${category.items.map(skill => `<span class="pill">${skill}</span>`).join('')}
+                </div>
             </div>
-        </div>
-    `).join('');   
+        `).join('');   
 
-   }
-
-    
+        // const experienceContainer = document.getElementById('experience-container');
+        // experienceContainer.innerHTML = app.resumeItems.experience.map(experience => `
+        //     <div class="experience-item">
+        //         <div class="exp-header">
+        //             <h3>${experience.title}</h3>
+        //             <span class="date">${experience.date}</span>
+        //         </div>
+        //         <ul>
+        //             ${experience.details.map(detail => `<li>${detail}</li>`).join('')}
+        //         </ul>
+        //     </div>
+        // `).join('');   
+    }
 })(window.app = window.app || {});
